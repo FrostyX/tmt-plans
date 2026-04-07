45ac9a9bf912b549304eb03d47a173cfc3891ce8
Jakub Kadlcik <frostyx@email.cz>
2026-04-08 00:40:47 +0200

    wip

--- tests/fedora-review/run-fedora-review.py
+++ tests/fedora-review/run-fedora-review.py
@@ -6,6 +6,7 @@ import os
 import subprocess
 import shutil
 from pathlib import Path
+from contextlib import suppress
 from enum import Enum
 import json
 import yaml
@@ -27,7 +28,7 @@ class Result(Enum):
     PASS = "pass"
 
 
-def dump_results_yaml(issues: int):
+def dump_results_yaml(issues: int, skipped: int):
     """
     https://tmt.readthedocs.io/en/stable/spec/results.html
     """
@@ -36,8 +37,11 @@ def dump_results_yaml(issues: int):
         {
             "name": "/",
             "result": result.value,
-            "note": [f"{issues} issues"],
-            "log": ["viewer.html"] + FEDORA_REVIEW_RESULTS,
+            "note": [
+                f"{skipped} skipped",
+                f"{issues} issues",
+            ],
+            "log": ["viewer.html", "skipped-checks.json"] + FEDORA_REVIEW_RESULTS,
         }
     ]
     path = os.path.join(os.environ.get("TMT_TEST_DATA"), "results.yaml")
@@ -72,6 +76,15 @@ def copy_viewer_html():
     shutil.copy(viewer, Path(os.environ["TMT_TEST_DATA"]) / viewer)
 
 
+def copy_mock_packit_yaml():
+    """
+    Copy a mock .packit.yaml to the plan data directory
+    """
+    filename = ".packit.yaml"
+    print(f"Copying {filename} to the plan data")
+    shutil.copy(filename, Path(os.environ["TMT_PLAN_DATA"]) / filename)
+
+
 def copy_data_into_data():
     """
     There is a weird bug that we discovered with @LecrisUT. For some reason,
@@ -98,7 +111,17 @@ def fedora_review(spec_file, workdir):
     name = Path(spec_file).stem
     cmd = ["fedora-review", "--prebuilt", "-n", name]
     print(f"Running: {" ".join(cmd)}")
-    subprocess.run(cmd, cwd=workdir, env=env, check=True)
+    proc = subprocess.run(
+        cmd,
+        cwd=workdir,
+        env=env,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    print(proc.stdout.decode("utf-8"))
+    print(proc.stderr.decode("utf-8"))
+    if proc.returncode:
+        raise RuntimeError("The fedora-review command failed")
 
     path = os.path.join(workdir, "review-" + name, "review.json")
     if not os.path.exists(path):
@@ -110,9 +133,50 @@ def fedora_review(spec_file, workdir):
     return review
 
 
-def count_issues(review):
+def count_issues(review, skip):
+    print(f"Skipping these checks: {skip}")
     issues = review.get("issues", [])
+    issues = [x for x in issues if x["name"] not in skip]
     return len(issues)
+
+
+def skip_checks(packit_yaml):
+    skip_for_all = [
+        # TODO why?
+        "CheckNoNameConflict",
+
+        # TODO why?
+        "CheckLicensInDoc",
+
+        # TODO why?
+        "CheckLicenseField",
+    ]
+    skip_for_package = []
+    if "fedora_review" in packit_yaml:
+        skip_for_package = packit_yaml["fedora_review"].get("skip", [])
+    return skip_for_all + skip_for_package
+
+
+def dump_skipped_checks(checks: list[str]) -> None:
+    path = "./skipped-checks.json"
+    with open(path, "w+") as fp:
+        json.dump(checks, fp)
+
+
+def parse_packit_yaml(workdir: Path):
+    accepted_filenames = [
+        "packit.yaml",
+        ".packit.yaml",
+        "packit.yml",
+        ".packit.yml",
+    ]
+    for filename in accepted_filenames:
+        packit_yaml_path = os.path.join(workdir, filename)
+        print(f"Looking for packit configuration: {packit_yaml_path}")
+        with suppress(FileNotFoundError), open(packit_yaml_path, "r") as fp:
+            print("Packit configuration found")
+            return yaml.safe_load(fp)
+    return {}
 
 
 def main(args: argparse.Namespace) -> None:
@@ -130,12 +194,19 @@ def main(args: argparse.Namespace) -> None:
     shutil.copy(args.spec_file, args.workdir)
 
     review = fedora_review(args.spec_file, args.workdir)
-    issues = count_issues(review)
-    dump_results_yaml(issues)
+    copy_mock_packit_yaml()
+
+    packit_yaml = parse_packit_yaml(args.workdir)
+    skip = skip_checks(packit_yaml)
+    dump_skipped_checks(skip)
+
+    issues = count_issues(review, skip)
+    dump_results_yaml(issues, len(skip))
     copy_fedora_review_results(args.spec_file, args.workdir)
     copy_viewer_html()
     copy_data_into_data()
 
+    print(f"Skipped {len(skip)} issues")
     print(f"Found {issues} issues")
     if issues:
         sys.exit(1)
